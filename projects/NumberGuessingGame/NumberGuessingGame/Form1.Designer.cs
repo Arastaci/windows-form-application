724584bcf1e2aab14dbc3a36738ee51f3ced2c80
Esat <120355601+Arastaci@users.noreply.github.com>
2024-10-22 00:48:29 +0300

    feat: add guess the number game

---
+++ projects/NumberGuessingGame/NumberGuessingGame/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace NumberGuessingGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtGuess = new System.Windows.Forms.TextBox();
+            this.btnGuess = new System.Windows.Forms.Button();
+            this.lblAttempts = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtGuess
+            // 
+            this.txtGuess.Location = new System.Drawing.Point(267, 183);
+            this.txtGuess.Name = "txtGuess";
+            this.txtGuess.Size = new System.Drawing.Size(159, 20);
+            this.txtGuess.TabIndex = 0;
+            // 
+            // btnGuess
+            // 
+            this.btnGuess.Location = new System.Drawing.Point(267, 262);
+            this.btnGuess.Name = "btnGuess";
+            this.btnGuess.Size = new System.Drawing.Size(75, 23);
+            this.btnGuess.TabIndex = 1;
+            this.btnGuess.Text = "Enter";
+            this.btnGuess.UseVisualStyleBackColor = true;
+            this.btnGuess.Click += new System.EventHandler(this.btnGuess_Click);
+            // 
+            // lblAttempts
+            // 
+            this.lblAttempts.AutoSize = true;
+            this.lblAttempts.Location = new System.Drawing.Point(264, 107);
+            this.lblAttempts.Name = "lblAttempts";
+            this.lblAttempts.Size = new System.Drawing.Size(48, 13);
+            this.lblAttempts.TabIndex = 2;
+            this.lblAttempts.Text = "Attempts";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblAttempts);
+            this.Controls.Add(this.btnGuess);
+            this.Controls.Add(this.txtGuess);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtGuess;
+        private System.Windows.Forms.Button btnGuess;
+        private System.Windows.Forms.Label lblAttempts;
+    }
+}
+
---
+++ projects/NumberGuessingGame/NumberGuessingGame/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace NumberGuessingGame
+{
+    public partial class Form1 : Form
+    {
+        private int randomNumber;  
+        private int attemptsLeft = 5; 
+        private int userGuess;        
+
+        public Form1()
+        {
+            InitializeComponent();
+            StartNewGame();  
+        }
+        private void StartNewGame()
+        {
+            Random random = new Random();
+            randomNumber = random.Next(1, 101); 
+            attemptsLeft = 5;
+            lblAttempts.Text = "Remaining attempts: " + attemptsLeft;
+        }
+
+        private void btnGuess_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(txtGuess.Text, out userGuess))
+            {
+                if (userGuess < 1 || userGuess > 100)
+                {
+                    MessageBox.Show("Please enter a number between 1 and 100.");
+                    return;
+                }
+
+                if (userGuess == randomNumber)
+                {
+                    MessageBox.Show("Congratulations! You guessed the correct number!", "Success");
+                    StartNewGame(); 
+                }
+                else
+                {
+                    attemptsLeft--;
+                    lblAttempts.Text = "Remaining attempts: " + attemptsLeft;
+
+                    if (attemptsLeft == 0)
+                    {
+                        MessageBox.Show($"You've run out of attempts! The correct number was {randomNumber}.", "Fail");
+                        StartNewGame(); 
+                    }
+                    else
+                    {
+                        if (userGuess < randomNumber)
+                        {
+                            MessageBox.Show("Your guess is too low. Try again!", "Hint");
+                        }
+                        else
+                        {
+                            MessageBox.Show("Your guess is too high. Try again!", "Hint");
+                        }
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please enter a valid number.");
+            }
+        }
+    }
+}
